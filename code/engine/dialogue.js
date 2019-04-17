190d7614cc95ca6e4f448e80aa7d6c51d2567b13
rowberry <49139043+rowberry@users.noreply.github.com>
2019-04-17 20:38:34 +1000

    Updated to include dialog box
    Dialog box code added

--- code/engine/dialogue.js
+++ code/engine/dialogue.js
@@ -8,6 +8,14 @@ var dialogueWalkAway = 100; //Distance that player must walk away for dialogue b
 var dialogueAlreadyEngaged = false; //Is the user holding down the talk key?
 var dialogueActive = false; //Is dialogue on screen?
 
+var dialogBox; //Object to draw dialogue
+var drawWidth;//Box width
+var drawHeight;//Box Height
+var swirlLength; //Style line length
+var lineStyleThick; //line thickness
+var diaBoxX; //X co-ordinate of dialogue box
+var diaBoxY; //Y co-ordinate of dialogue box
+
 /* Reads the level dialogue from an external JSON file. 
  * This is currently non-functional. 
  */
@@ -19,60 +27,72 @@ function parseLevelDialogue() {
 	levelJSON = createThis.cache.json.get('levelJSON');
 	dialogue = levelJSON.dialogue;
 	dialogueMax = dialogue.length - 1;
+	dialogBox = createThis.add.graphics();
 }
 
-/*function shrineGroup()
+function drawDialogueBox()
 {
-	createThis.shrineItems.add.group(
-	{
-		key: 'medea',
-		setXY:
-		{
-			x:160
-			y:780
-		}
-	},
-	{
-		key: 'shrineJason',
-		setXY:
-		{
-			x:290
-			y:780
-		}
-	},
-	{
-		key: 'shrinePortal',
-		setXY:
-		{
-			x:1630
-			y:780
-		}
-	});
-}
-
-//in create
-function shrineInteractive()
-{
-	Phaser.Actions.call(createThis.shrineItems.getChildren(), function(sitem){}, createThis);
 	
-	sitem.setInteractive();
+	drawWidth = createThis.sys.game.config.width;
+	drawHeight = createThis.sys.game.config.height*0.30;
+	swirlLength = drawWidth*0.02;
+	lineStyleThick = swirlLength/5;
+	diaBoxX = createThis.cameras.main.scrollX;
+	diaBoxY = createThis.cameras.main.scrollY + createThis.sys.game.config.height*0.70;
+	
+	//draw outer rectangle
+	dialogBox.lineStyle(lineStyleThick,0x000000,1);	
+	dialogBox.fillStyle(0xf2edaa,1);
+	dialogBox.fillRect(diaBoxX,diaBoxY,drawWidth,drawHeight);
+	dialogBox.strokeRect(diaBoxX,diaBoxY,drawWidth,drawHeight);
+	
+	
+	//styling
+	//Top LHS
+	dialogBox.beginPath();
+	dialogBox.moveTo(diaBoxX+swirlLength,diaBoxY);
+	dialogBox.lineTo(diaBoxX+swirlLength,diaBoxY+swirlLength);
+	dialogBox.lineTo(diaBoxX+(swirlLength/2),diaBoxY+swirlLength);
+	dialogBox.lineTo(diaBoxX+(swirlLength/2),diaBoxY+(swirlLength/2));
+	dialogBox.strokePath();
 
-	sitem.on('pointerdown', function()
-	{
-		if (item.texture.key === 'medea')
-		{
-			talkMedea = createThis.add.text(160,550,'Talk',{color: '#ff00ff'});
-		}
-		else if (item.texture.key === 'shrineJason')
-		{
-			//do something else
-		}
-	});
+	//Bottom LHS
+	dialogBox.beginPath();
+	dialogBox.moveTo(diaBoxX+swirlLength,drawHeight+diaBoxY);
+	dialogBox.lineTo(diaBoxX+swirlLength,drawHeight+diaBoxY-swirlLength);
+	dialogBox.lineTo(diaBoxX+(swirlLength/2),drawHeight+diaBoxY-swirlLength);
+	dialogBox.lineTo(diaBoxX+(swirlLength/2),drawHeight+diaBoxY-(swirlLength/2));
+	dialogBox.strokePath();
+
+	//Top RHS
+	dialogBox.beginPath();
+	dialogBox.moveTo(diaBoxX+drawWidth-swirlLength,diaBoxY);
+	dialogBox.lineTo(diaBoxX+drawWidth-swirlLength,diaBoxY+swirlLength);
+	dialogBox.lineTo(diaBoxX+drawWidth-(swirlLength/2),diaBoxY+swirlLength);
+	dialogBox.lineTo(diaBoxX+drawWidth-(swirlLength/2),diaBoxY+(swirlLength/2));
+	dialogBox.strokePath();
+
+	//Bottom RHS
+	dialogBox.beginPath();
+	dialogBox.moveTo(diaBoxX+drawWidth-swirlLength,drawHeight+diaBoxY);
+	dialogBox.lineTo(diaBoxX+drawWidth-swirlLength,drawHeight+diaBoxY-swirlLength);
+	dialogBox.lineTo(diaBoxX+drawWidth-(swirlLength/2),drawHeight+diaBoxY-swirlLength);
+	dialogBox.lineTo(diaBoxX+drawWidth-(swirlLength/2),drawHeight+diaBoxY-(swirlLength/2));
+	dialogBox.strokePath();
+
+	//Style Lines
+	//Top
+	dialogBox.lineBetween(diaBoxX+swirlLength,diaBoxY+(swirlLength/2),diaBoxX+drawWidth-swirlLength,diaBoxY+(swirlLength/2));
+	//Bottom
+	dialogBox.lineBetween(diaBoxX+swirlLength,diaBoxY+drawHeight-(swirlLength/2),diaBoxX+drawWidth-swirlLength,diaBoxY+drawHeight-(swirlLength/2));
+	//Left
+	dialogBox.lineBetween(diaBoxX+(swirlLength*0.75),diaBoxY+swirlLength,diaBoxX+(swirlLength*0.75),diaBoxY+drawHeight-swirlLength);
+	//Right
+	dialogBox.lineBetween(diaBoxX+drawWidth-(swirlLength*0.75),diaBoxY+swirlLength,diaBoxX+drawWidth-(swirlLength*0.75),diaBoxY+drawHeight-swirlLength);
+	
 }
 
-function shrineText()
+function clearDialogueBox ()
 {
-	talkMedea.setText(debug);
+	dialogBox.clear();
 }
-
-*/
