b207658e307c44189729cddbed05cdfbd4c7ce16
ShivaBit1 <49138897+ShivaBit1@users.noreply.github.com>
2019-05-15 07:25:41 +1000

    Add files via upload

--- code/engine/endScreen.js
+++ code/engine/endScreen.js
@@ -1,20 +1,33 @@
+
 class endScreen extends Phaser.Scene {
 	constructor() {
-		super({ key: "endScreen" });	
+		super({ key: "endScreen", active: false});	
+	}
+
+	preload()
+	{
+		//end game image
+		this.load.image('endbg', 'assets/stage/background/endbg.png');
+		this.load.image('menubut', 'assets/stage/background/menubut.png');
 	}
 	
 	create(){ //creating end screen 
-	
+		createThis = this;
+        currentLevelID = 'endScreen';
 		//images
 		
-		this.add.image(0,0, "endbg").setOrigin(0);.setDepth(0);
+		this.add.image(0,0, "endbg").setOrigin(0).setDepth(0);
 		
-		this.add.image(this.game.renderer.width / 2, this.game.renderer.height / 2, "menubut").setDepth(1);
+		let toMenuButton = this.add.image(this.game.renderer.width / 2, this.game.renderer.height / 2, "menubut").setDepth(1).setInteractive();
 		
+		
+		
+		toMenuButton.on("pointerup", ()=>{
+			this.scene.start('titleScreen');
+		
+	});
 	}
-	
-	playButton.setInteractive();
+}
 
-	
 		
 
