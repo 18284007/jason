61fc7724a5ebee6811029e97753566be174df79a
JohnAlexakis <49139064+JohnAlexakis@users.noreply.github.com>
2019-05-15 11:52:17 +1000

    Update titleScreen.js

--- code/engine/Levels/titleScreen.js
+++ code/engine/Levels/titleScreen.js
@@ -7,9 +7,9 @@ class titleScreen extends Phaser.Scene{
 
 	preload()
 	{
-        createThis = this;
-        currentLevelID = 'titleScreen';
-	
+        	createThis = this;
+        	currentLevelID = 'titleScreen';
+		
 		
 		this.load.image('titlebg', 'assets/stage/background/titlebg.jpg');
 		this.load.image('playbut', 'assets/stage/background/playbut.png');
@@ -20,27 +20,24 @@ class titleScreen extends Phaser.Scene{
 
 	create()
 	{
+		
  		console.log(this);
 		this.add.image(0,0, "titlebg").setOrigin(0).setDepth(0);
 		//let playButton = this.add.image(this.game.renderer.width /2, this.game.renderer.height * 0.80, "playbut").setDepth(1);
 		
-		
-		this.playButton = this.add.sprite(this.game.renderer.width /2, this.game.renderer.height * 0.80, 'playbut').setInteractive();
-    		this.playButton.on('pointerover', function (event) { /* Do something when the mouse enters */ });
-   		this.playButton.on('pointerout', function (event) { /* Do something when the mouse exits. */ });
-    		this.playButton.on('pointerdown', startGame); // Start game on click.
-		
-		
+		let startGame = this.add.image(this.game.renderer.width / 2, this.game.renderer.height *0.80, "playbut").setDepth(1).setInteractive();
+			
+		startGame.on("pointerup", ()=>{
+		this.scene.start('argoLanding');
+		this.sound.stop('water');
+		});
+			
 		this.sound.pauseOnBlur = false;
 		this.sound.play('water',
 		{loop: true});	
 		
 
 		
-		function startGame(tempNewLevelID) {
-		var oldLevelID = currentLevelID;
-		game.scene.run(tempNewLevelID);
-		game.scene.stop(oldLevelID);
-}
+		
 }
 }
