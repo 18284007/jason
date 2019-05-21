eda40ab21afe46a4d75918948671935e0d869a90
rowberry <49139043+rowberry@users.noreply.github.com>
2019-05-21 18:23:16 +1000

    fixed bugs - errors changing from title screens

--- code/engine/Levels/endScreen.js
+++ code/engine/Levels/endScreen.js
@@ -6,26 +6,25 @@ class endScreen extends Phaser.Scene {
 
 	preload()
 	{
+		createThis = this;
+        currentLevelID = 'endScreen';
 		//end game image
 		this.load.image('endbg', 'assets/stage/background/endbg.png');
 		this.load.image('menubut', 'assets/stage/background/menubut.png');
 	}
 	
 	create(){ //creating end screen 
-		createThis = this;
-        currentLevelID = 'endScreen';
 		//images
 		
 		this.add.image(0,0, "endbg").setOrigin(0).setDepth(0);
-		
-		let toMenuButton = this.add.image(this.game.renderer.width / 2, this.game.renderer.height / 2, "menubut").setDepth(1).setInteractive();
-		
+		let toMenuButton = this.add.image(this.game.renderer.width*0.5, this.game.renderer.height*0.5, "menubut").setDepth(1).setInteractive();
 		
 		
 		toMenuButton.on("pointerup", ()=>{
-			this.scene.start('titleScreen');
+			changeLevel('titleScreen');
 		
 	});
+		
 	}
 }
 
