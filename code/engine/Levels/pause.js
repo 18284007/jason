034e21efd036ac2e1d8d6c98ffbfe4a66d1cd909
ShivaBit1 <49138897+ShivaBit1@users.noreply.github.com>
2019-05-21 16:55:30 +1000

    Add files via upload

--- code/engine/Levels/pause.js
+++ code/engine/Levels/pause.js
@@ -7,16 +7,18 @@ class pause extends Phaser.Scene {
 	{
 		//pause image
 		
-		this.load.image('menubut', 'assets/stage/background/menubut.png');
+		this.load.image('resumebut', 'assets/stage/background/resumebut.png');
+		this.load.image('pausebg', 'assets/stage/background/pausebg.png');
 	}
 	
 	create(){ 
 		createThis = this;
         game.scene.pause(currentLevelID);
 		
-		this.add.image(0,0, "menubut").setOrigin(0).setDepth(0);
+		this.add.image(0,0, "pausebg").setOrigin(0).setDepth(0);		
+		this.add.image(0,0, "resumebut").setOrigin(0).setDepth(0);
 		
-		let toPauseButton = this.add.image(this.game.renderer.width / 2, this.game.renderer.height / 2, "menubut").setDepth(1).setInteractive();
+		let toPauseButton = this.add.image(this.game.renderer.width / 2, this.game.renderer.height / 2, "resumebut").setDepth(1).setInteractive();
 		
 		 toPauseButton.on('pointerup', function () {
 			
