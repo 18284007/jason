117f5f2ac4bcec0516027853c2271acadc842524
ShivaBit1 <49138897+ShivaBit1@users.noreply.github.com>
2019-05-24 21:31:16 +1000

    Add files via upload

--- code/engine/Levels/gardenDungeon.js
+++ code/engine/Levels/gardenDungeon.js
@@ -11,11 +11,27 @@ class gardenDungeon extends Phaser.Scene{
         currentLevelID = 'gardenDungeon';
         backgroundLayer0 = 'bgDungeon';
         commonPreload();
+		
+		this.load.spritesheet('bats','assets/enemy/bats.png',
+		{	frameWidth: 100, frameHeight: 140});			
 	}
 
 	create()
 	{
         loadMap();
+		
+		createThis.anims.create({
+        key: 'batsLeft',
+        frames: createThis.anims.generateFrameNumbers('bats', { start: 0, end: 0 }),
+        frameRate: 10,
+        repeat: -1
+    });
+		createThis.anims.create({
+        key: 'batsRight',
+        frames: createThis.anims.generateFrameNumbers('bats', { start: 1, end: 1 }),
+        frameRate: 10,
+        repeat: -1
+    });
     }
 
     update()
