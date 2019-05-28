b83b3475b29793f325925bb208944fc688d72a56
ShivaBit1 <49138897+ShivaBit1@users.noreply.github.com>
2019-05-28 13:18:23 +1000

    Update pause.js

--- code/engine/Levels/pause.js
+++ code/engine/Levels/pause.js
@@ -9,6 +9,7 @@ class pause extends Phaser.Scene {
 		
 		this.load.image('resumebut', 'assets/stage/background/resumebut.png');
 		this.load.image('pausebg', 'assets/stage/background/pausebg.png');
+		this.load.image('mapMenu', 'assets/stage/background/mapMenu.png');		
 	}
 	
 	create(){ 
@@ -16,23 +17,28 @@ class pause extends Phaser.Scene {
         game.scene.pause(currentLevelID);
 		
 		this.add.image(0,0, "pausebg").setOrigin(0).setDepth(0);		
-		this.add.image(0,0, "resumebut").setOrigin(0).setDepth(0);
+	
 		
-		let toPauseButton = this.add.image(this.game.renderer.width / 2, this.game.renderer.height / 2, "resumebut").setDepth(1).setInteractive();
+		let toPauseButton = this.add.image(this.game.renderer.width / 2, this.game.renderer.height * 0.5, "resumebut").setDepth(1).setInteractive();
+		let toMapMenu = this.add.image(this.game.renderer.width / 2, this.game.renderer.height * 0.7, "mapMenu").setDepth(1).setInteractive();		
 		
 		 toPauseButton.on('pointerup', function () {
-			
-		
-			 
-				
-				
-			
-			 
-			
 		 game.scene.resume(currentLevelID);
-		 game.scene.stop('pause');
+		 game.scene.stop('pause');			
 		
-		});
+		});	 
+				
+		 toMapMenu.on('pointerup', ()=>{
+		 changeLevel('mapMenu');
+		 game.scene.stop('pause');			
+		
+		});	 				
+			
+			 
+			
+
+		
+		
 
 	}
 }	
