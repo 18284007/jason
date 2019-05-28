00c58b0e0fa6dc5a6ed7fab21e42665586f62768
18284007 <49138884+18284007@users.noreply.github.com>
2019-05-28 20:20:32 +1000

    enemy.js - Fix Undefined Crash When Moving Through Portal

--- code/engine/enemy.js
+++ code/engine/enemy.js
@@ -628,7 +628,7 @@ class spiderBoss extends enemyBase {
 function enemyMovement() {
 	if (enemyCount > 0){
 		for (i = 0; i < enemyCount; i++){
-			if (enemies[i].alive){
+			if (enemies[i].alive && enemies[i].body !== undefined){
 				enemies[i].movement();	
 				enemies[i].update();
 			}
