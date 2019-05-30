20d0953d9f29509a4dad92cd7a85c97007ae3813
rowberry <49139043+rowberry@users.noreply.github.com>
2019-05-30 12:59:31 +1000

    github did not correctly update previously

--- code/engine/Levels/colchisFields.js
+++ code/engine/Levels/colchisFields.js
@@ -16,6 +16,18 @@ class colchisFields extends Phaser.Scene{
 	create()
 	{
         loadMap();
+	createThis.anims.create({
+            key: 'kingIdleLeft',
+            frames: createThis.anims.generateFrameNumbers('kingSprite', { start: 0, end: 0 }),
+            frameRate: 10,
+            repeat: -1
+        });
+        createThis.anims.create({
+            key: 'kingIdleRight',
+            frames: createThis.anims.generateFrameNumbers('kingSprite', { start: 1, end: 1 }),
+            frameRate: 10,
+            repeat: -1
+        });
     }
 
     update()
