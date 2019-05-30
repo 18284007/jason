ecfb20b4ec3b42cb728f081d74cd33c50a2315c8
rowberry <49139043+rowberry@users.noreply.github.com>
2019-05-30 13:49:32 +1000

    updated to latest

--- code/engine/Levels/gardenForest.js
+++ code/engine/Levels/gardenForest.js
@@ -11,16 +11,36 @@ class gardenForest extends Phaser.Scene{
         currentLevelID = 'gardenForest';
         backgroundLayer0 = 'bgForest';
         commonPreload();
+		
+		this.load.spritesheet('snake','assets/enemy/snake.png',
+		{	frameWidth: 120, frameHeight: 83});	
+			
+
 	}
 
 	create()
 	{
 
-        loadMap();
+        loadMap();  
+
+		createThis.anims.create({
+        key: 'snakeLeft',
+        frames: createThis.anims.generateFrameNumbers('snake', { start: 0, end: 0 }),
+        frameRate: 10,
+        repeat: -1
+    });
+		createThis.anims.create({
+        key: 'snakeRight',
+        frames: createThis.anims.generateFrameNumbers('snake', { start: 1, end: 1 }),
+        frameRate: 10,
+        repeat: -1
+    });
+	
     }
 
     update()
     {
-        callUpdateFuncs();
+        callUpdateFuncs(); 
+		
     }
 }
