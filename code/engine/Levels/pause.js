3dd8ed26a8c2f9fa1b7f42947a0a7ee3d5a2b270
rowberry <49139043+rowberry@users.noreply.github.com>
2019-06-06 10:09:52 +1000

    mute button added

--- code/engine/Levels/pause.js
+++ code/engine/Levels/pause.js
@@ -9,19 +9,37 @@ class pause extends Phaser.Scene {
 		
 		this.load.image('resumebut', 'assets/stage/background/resumebut.png');
 		this.load.image('pausebg', 'assets/stage/background/pausebg.png');
-		this.load.image('mapMenu', 'assets/stage/background/mapMenu.png');		
+		this.load.image('mapMenu', 'assets/stage/background/mapMenu.png');
+		this.load.image('mutebtn', 'assets/stage/background/mutebtn.png');		
 	}
 	
 	create(){ 
 		//createThis = this;
         game.scene.pause(currentLevelID);
+
+
+        this.drawPauseSurface = this.add.graphics();
+
+        this.drawPauseSurface.lineStyle(this.sys.game.config.width*0.004,0x000000,1);	
+		this.drawPauseSurface.fillStyle(0xf2edaa,1);
+		this.drawPauseSurface.fillRect(this.sys.game.config.width*0.2,this.sys.game.config.height*0.1,this.sys.game.config.width*0.6,this.sys.game.config.height*0.7);
+		this.drawPauseSurface.strokeRect(this.sys.game.config.width*0.2,this.sys.game.config.height*0.1,this.sys.game.config.width*0.6,this.sys.game.config.height*0.7);
 		
 		this.add.image(0,0, "pausebg").setOrigin(0).setDepth(0);		
 	
 		
 		let toPauseButton = this.add.image(this.game.renderer.width / 2, this.game.renderer.height * 0.5, "resumebut").setDepth(1).setInteractive();
-		let toMapMenu = this.add.image(this.game.renderer.width / 2, this.game.renderer.height * 0.7, "mapMenu").setDepth(1).setInteractive();		
+		let toMapMenu = this.add.image(this.game.renderer.width / 2, this.game.renderer.height * 0.7, "mapMenu").setDepth(1).setInteractive();
+		let muteBtn = this.add.image(this.game.renderer.width*0.78, this.game.renderer.height * 0.135, "mutebtn").setDepth(1).setInteractive();		
 		
+		if (!musicMuted)
+		{
+			muteBtn.setTint(0x00ff00);
+		}else
+		{
+			muteBtn.setTint(0xff0000);
+		}
+
 		 toPauseButton.on('pointerup', function () {
 		 game.scene.resume(currentLevelID);
 		 game.scene.stop('pause');			
@@ -34,7 +52,20 @@ class pause extends Phaser.Scene {
 		
 		});	 				
 			
-			 
+		muteBtn.on('pointerup', ()=>{
+		 if (!musicMuted)
+		 {
+		 	music.setMute(true);
+		 	musicMuted = true;
+		 	muteBtn.setTint(0xff0000);
+		 }else
+		 {
+		 	music.setMute(false);
+		 	musicMuted = false;
+		 	muteBtn.setTint(0x00ff00);
+		 }			
+		
+		});		 
 			
 
 		
