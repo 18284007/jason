c38635a3b96c113c84fd8ad68fe50bb4d512ef8d
18284007 <49138884+18284007@users.noreply.github.com>
2019-06-11 15:52:08 +1000

    healthBar.js - format

--- code/engine/healthBar.js
+++ code/engine/healthBar.js
@@ -9,86 +9,71 @@ var hbIncrement;
 var hbReady;
 var intervalVar;
 
-function firstInitHealthBar()
-{
-	hbWidth = userIntThis.sys.game.config.width*0.20;
-	hbHeight = userIntThis.sys.game.config.height*0.05;
-	hbIncrement = hbWidth/maxHealth;
-	hbReady = true;
-	oldHealth = maxHealth;
-	healthBar = userIntThis.add.graphics();
-	healthBar.setDepth(500);
-}
-function maxHealthUpdate()
-{
-	hbIncrement = hbWidth/maxHealth;	
+function firstInitHealthBar() {
+    hbWidth = userIntThis.sys.game.config.width*0.20;
+    hbHeight = userIntThis.sys.game.config.height*0.05;
+    hbIncrement = hbWidth/maxHealth;
+    hbReady = true;
+    oldHealth = maxHealth;
+    healthBar = userIntThis.add.graphics();
+    healthBar.setDepth(500);
 }
 
-function healthBarReset()
-{
-	oldHealth = maxHealth;
-	healthDif = 0;
-	if (intervalVar !== undefined)
-	{
-		clearInterval(intervalVar);
-	}
+function maxHealthUpdate() {
+    hbIncrement = hbWidth/maxHealth;    
 }
 
-function parseHealthBarAnimate()
-{
-	hbReady = true;
-	healthDif = oldHealth - currentHealth;
-	if(healthDif != 0)
-	{
-		intervalVar = setInterval(oldHealthCtr, 50);
-	}
+function healthBarReset() {
+    oldHealth = maxHealth;
+    healthDif = 0;
+    if (intervalVar !== undefined) {
+        clearInterval(intervalVar);
+    }
 }
 
-function oldHealthCtr()
-{
-	if (healthDif > 0)
-	{
-		healthDif--;
-		oldHealth--;
-	}
-	else if (healthDif < 0)
-	{
-		healthDif++;
-		oldHealth++;
-	}
-	else
-	{
-		clearInterval(intervalVar);
-	}
-	drawHealthBar();
+function parseHealthBarAnimate() {
+    hbReady = true;
+    healthDif = oldHealth - currentHealth;
+    if (healthDif != 0) {
+        intervalVar = setInterval(oldHealthCtr, 50);
+    }
 }
 
-function parseHealthBar()
-{
-	if (hbReady)
-	{
-		hbReady = false;
-		drawHealthBar();
-		setTimeout(setHBReady(), 250);
-	}
+function oldHealthCtr() {
+    if (healthDif > 0) {
+        healthDif--;
+        oldHealth--;
+    } else if (healthDif < 0) {
+        healthDif++;
+        oldHealth++;
+    } else {
+        clearInterval(intervalVar);
+    }
+
+    drawHealthBar();
 }
 
-function drawHealthBar()
-{
-	healthBar.clear();
-	hbX = userIntThis.cameras.main.scrollX + userIntThis.sys.game.config.width*0.775;
-	hbY = userIntThis.cameras.main.scrollY + userIntThis.sys.game.config.height*0.0225;
-	healthBar.lineStyle(1,0x000000,1);
-	healthBar.fillStyle(0xff0000,1);
-	healthBar.strokeRect(hbX,hbY,hbWidth,hbHeight);
-	if (currentHealth > 0)
-	{
-		healthBar.fillRect(hbX,hbY,hbIncrement*oldHealth,hbHeight);
-	}
-	
+function parseHealthBar() {
+    if (hbReady) {
+        hbReady = false;
+        drawHealthBar();
+        setTimeout(setHBReady(), 250);
+    }
 }
 
-function setHBReady()
-{
-	hbReady = true;
+function drawHealthBar() {
+    healthBar.clear();
+    hbX = userIntThis.cameras.main.scrollX + userIntThis.sys.game.config.width*0.775;
+    hbY = userIntThis.cameras.main.scrollY + userIntThis.sys.game.config.height*0.0225;
+    healthBar.lineStyle(1,0x000000,1);
+    healthBar.fillStyle(0xff0000,1);
+    healthBar.strokeRect(hbX,hbY,hbWidth,hbHeight);
+    
+    if (currentHealth > 0) {
+        healthBar.fillRect(hbX,hbY,hbIncrement*oldHealth,hbHeight);
+    }
+}
+
+function setHBReady() {
+    hbReady = true;
 }
